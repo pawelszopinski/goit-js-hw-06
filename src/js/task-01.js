418bd85d1b57050f7c243424c2e703ccde3fdd66
Paweł Szopiński <szopinskipawel@gmail.com>
2023-03-09 17:38:00 +0100

    Task 1 in progress

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+const list = document.getElementsByTagName("h2");
+const listLength = list.length
+const categories = `Number of categories: ${listLength}`
+console.log(categories)
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
