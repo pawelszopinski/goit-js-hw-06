7606cee7386ec5c1a1ff34ad6bd27dd9bd6ffe99
Paweł Szopiński <szopinskipawel@gmail.com>
2023-03-09 22:34:31 +0100

    Task 1 completed

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,20 @@
 const list = document.getElementsByTagName("h2");
-const listLength = list.length
-const categories = `Number of categories: ${listLength}`
-console.log(categories)
+const listLength = list.length;
+const categories = `Number of categories: ${listLength}`;
+const category = `Category: `;
+const elements = `Elements: `;
+const listElements = document.getElementsByTagName("ul");
+console.log(`${categories}
+
+${category} ${list[0].textContent}
+${elements} ${listElements[1].childElementCount}
+
+${category} ${list[1].textContent}
+${elements} ${listElements[2].childElementCount}
+
+${category} ${list[2].textContent}
+${elements} ${listElements[3].childElementCount}`);
+
 // Number of categories: 3
 
 // Category: Animals
