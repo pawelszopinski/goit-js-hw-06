38859bc79bded4302ba7002b050f79ea89676a33
Paweł Szopiński <szopinskipawel@gmail.com>
2023-03-13 13:45:05 +0100

    Task 2 completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list= document.getElementById('ingredients') 
+for(let i=0;i<ingredients.length;i++) {
+const elements = document.createElement("li")
+elements.className = "item"
+console.log(elements.outerHTML)
+elements.textContent = ingredients[i]
+list.append(elements)
+}
